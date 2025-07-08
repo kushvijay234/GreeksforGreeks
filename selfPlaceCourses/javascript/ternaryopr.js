2f85f9de209fb1a2057a4da1d89c39f73ff97762
Vijay Kushwaha <kushwahavijay234@gmail.com>
2025-07-08 19:08:36 +0530

    nested Ternary Operator

--- selfPlaceCourses/javascript/ternaryopr.js
+++ selfPlaceCourses/javascript/ternaryopr.js
@@ -2,11 +2,15 @@
 * Ternary Operators 
 */
 
-const totalMarksScored = 60;
+ const totalMarksScored = 100;
 
 // (totalMarksScored < 40) ? console.log("You Need to Word Hard") : console.log("You Cleared the exam. Great!")
+ const result = totalMarksScored < 40 ? "You need to Work Hard" : " You Cleared the Exam. Great !"
 
-const result = totalMarksScored < 40 ? "You need to Work Hard" : " You Cleared the Exam. Great !"
+console.log(result) 
 
-console.log(result)
+// PART -2 
 
+const result1 = totalMarksScored < 40 ? "You need to work Hard" : totalMarksScored < 60 ? "B Brade" : totalMarksScored < 75 ? "A grade" : totalMarksScored < 85 ? "A+ Grade" : "Genius";  
+
+console.log(result1);
