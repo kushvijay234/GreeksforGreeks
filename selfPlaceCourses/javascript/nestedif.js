6e23006506acda7667bd86adebb47c087bed1802
Vijay Kushwaha <kushwahavijay234@gmail.com>
2025-07-08 18:22:23 +0530

    nested if else

--- selfPlaceCourses/javascript/nestedif.js
+++ selfPlaceCourses/javascript/nestedif.js
@@ -6,11 +6,23 @@ const readlineSync = require("readline-sync");
 
 const number = Number(readlineSync.question("Enter a number"));
 
-const remainder =  number % 2;
+const remainder = number % 2;
 
 if (remainder === 0) {
     console.log(`${number} is an even number`)
+    // Nested Condition
+    if (number % 4 === 0) {
+        console.log(`${number} is divisible by 4`);
+    } else {
+        console.log(`${number} is not divisible by 4`)
+    }
 }
+
 else {
     console.log(`${number} is an odd number`);
+    if(number % 5 === 0) {
+        console.log(`${number} is divisible by 5`);
+    }else {
+        console.log(`${number} is not divisible by 5`);
+    }
 }
