0f9814cbbe61f78138a36f04825346890be87a79
Vijay Kushwaha <kushwahavijay234@gmail.com>
2025-07-08 18:57:46 +0530

    ternaryopr code

---
+++ selfPlaceCourses/javascript/ternaryopr.js
@@ -0,0 +1,12 @@
+/* 
+* Ternary Operators 
+*/
+
+const totalMarksScored = 60;
+
+// (totalMarksScored < 40) ? console.log("You Need to Word Hard") : console.log("You Cleared the exam. Great!")
+
+const result = totalMarksScored < 40 ? "You need to Work Hard" : " You Cleared the Exam. Great !"
+
+console.log(result)
+
