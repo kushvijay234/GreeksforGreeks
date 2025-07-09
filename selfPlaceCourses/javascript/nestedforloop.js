c6bcb5a795c904252b5542f8ee5c21ced3490198
Vijay Kushwaha <kushwahavijay234@gmail.com>
2025-07-09 11:30:53 +0530

    nestedforloop

---
+++ selfPlaceCourses/javascript/nestedforloop.js
@@ -0,0 +1,16 @@
+/* 
+* Nested For Loop
+*
+*/
+
+for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+        // console.log(`The Value of i is ${i} and j is ${j}`);
+
+        // table 
+        let product = i * j ;
+        console.log(`${i} X ${j} = ${product}`) 
+    }
+    console.log("---------------------------");
+}
+
