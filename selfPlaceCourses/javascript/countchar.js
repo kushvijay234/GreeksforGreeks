20b7ba021c34e38d729ecbdfdff8b0a5b189e174
Vijay Kushwaha <kushwahavijay234@gmail.com>
2025-07-09 11:57:16 +0530

    Count Char in String

---
+++ selfPlaceCourses/javascript/countchar.js
@@ -0,0 +1,12 @@
+/* 
+* Count Char in String
+*/
+
+let userName = "Vijay Kumar Kushwaha";
+
+let count = 0;
+
+for (let i = 0; i < userName.length; i++) {
+    count++;
+}
+console.log(`Number of Character in the string is ${count}`);
